662c08bd8bc3cdafaa0729d946ca6926e6deaade
minamahmood83 <minamahmood83@gmail.com>
2021-04-27 22:07:00 -0700

    add viewby role function

--- server.js
+++ server.js
@@ -115,3 +115,33 @@ function viewByDepartment() {
             });
     });
 }
+
+function viewByRole() {
+    connection.query("SELECT title FROM role", function(err, resolts) {
+        if (err) throw err;
+        inquirer
+            .prompt([{
+                type: "rawlist",
+                name: "choice",
+                choices: function() {
+                    let choiceArr = [];
+                    for (i = 0; i < results.length; i++) {
+                        choicesArr.push(resolts[i].title);
+                    }
+                    return choicesArr;
+                },
+                massage: "select role",
+            }, ])
+            .then(function(answer) {
+                console.log(answer.choice);
+                connection.query(
+                    "SELECT e.id AS ID, e.first_name AS First, e.last_name AS Last, e.role_id AS Role, r.salary, m.last_name AS Manager, d.name AS Department FROM employee e LEFT JOIN employee m.ON e.manager_id m.id LEFT JOIN role r ON e.role_id = r.title LEFT JOIN department d ON r.department_id =  d.id WHERE d.name =?", [answer.choice],
+                    function(err, results) {
+                        if (err) throw err;
+                        console.table(resolts);
+                        start();
+                    }
+                );
+            });
+    });
+}
